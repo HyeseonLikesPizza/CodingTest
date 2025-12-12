a6332d5d32f0304cdc770a6feb600dd50b51db9a
HyeseonLikesPizza <57754902+HyeseonLikesPizza@users.noreply.github.com>
2025-12-12 22:05:25 +0900

    [Bronze V] Title: 제리와 톰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+int main()
+{
+    int A,B,P,Q;
+
+    cin >> A >> B;
+    cout << B-A << " "<< B;
+
+    return 0;
+}
