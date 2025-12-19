65128cb1e734f6238e6126d9b77064a43f9a3cd7
HyeseonLikesPizza <57754902+HyeseonLikesPizza@users.noreply.github.com>
2025-12-19 21:36:15 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <string>
+#include <sstream>
+#include <queue>
+
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    
+    cout << "고려대학교";
+    
+
+    return 0;
+}
