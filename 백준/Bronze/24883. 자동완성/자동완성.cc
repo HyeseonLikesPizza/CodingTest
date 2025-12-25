cfe02766ba521019bc2fb8f4a0b7824ae93cc4d9
HyeseonLikesPizza <57754902+HyeseonLikesPizza@users.noreply.github.com>
2025-12-25 23:00:04 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <stack>
+#include <climits>
+
+using namespace std;
+
+
+int main()
+{
+    char c;
+
+    cin >> c;
+
+    if (c=='N' || c=='n')
+        cout << "Naver D2";
+    else
+        cout << "Naver Whale";
+    
+    return 0;
+}
