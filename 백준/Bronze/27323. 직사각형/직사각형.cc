b629c8119ad1d208853177647dc213f7b230db7b
HyeseonLikesPizza <57754902+HyeseonLikesPizza@users.noreply.github.com>
2025-12-29 22:40:37 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <stack>
+#include <climits>
+#include <set>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+    cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+    
+    int a,b;
+    cin >> a;
+    cin >> b;
+    cout << a*b;
+
+    return 0;
+}
