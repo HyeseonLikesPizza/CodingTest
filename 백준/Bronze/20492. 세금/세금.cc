0724865d3fc9c4803940114b3c448f4d6da28a4b
HyeseonLikesPizza <57754902+HyeseonLikesPizza@users.noreply.github.com>
2026-01-03 22:18:49 +0900

    [Bronze V] Title: 세금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20492. 세금/세금.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <stack>
+#include <climits>
+#include <set>
+#include <queue>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+
+int main()
+{
+    cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+    
+    int N;
+
+    cin >> N;
+
+    cout << fixed;
+    cout << setprecision(0);
+    cout << N * 0.78 << " " << N * 0.956;
+
+    return 0;
+}
