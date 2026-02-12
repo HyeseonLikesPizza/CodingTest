bbdc363ac1553f43c511430cd59e2f4cf811855d
HyeseonLikesPizza <57754902+HyeseonLikesPizza@users.noreply.github.com>
2026-02-12 14:28:38 +0900

    [Bronze III] Title: 사격 내기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27960. 사격 내기/사격 내기.cc
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <cstdio>
+#include <stdio.h>
+#include <climits>
+#include <queue>
+#include <tuple>
+#include <cmath>
+#include <map>
+
+using namespace std;
+int N, K;
+
+
+int main()
+{
+    cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+
+    int A,B,C;
+
+    cin >> A >> B;
+
+    C = A^B;
+    cout << C;
+    
+    return 0;
+}
