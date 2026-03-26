503c4951ab456029188c0ce1b3190b6581453931
임혜선 <57754902+HyeseonLikesPizza@users.noreply.github.com>
2026-03-26 23:53:25 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <cstdio>
+#include <stdio.h>
+#include <climits>
+#include <queue>
+#include <tuple>
+#include <cmath>
+#include <map>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+
+int main()
+{
+    cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+
+    int n;
+
+    cin >> n;
+
+    for (int i=0; i<n; i++)
+    {
+        cout << "LoveisKoreaUniversity ";
+    }
+    
+    return 0;
+}
