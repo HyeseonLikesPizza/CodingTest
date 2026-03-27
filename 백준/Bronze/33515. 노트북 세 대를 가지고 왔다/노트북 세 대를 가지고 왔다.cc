17da6d2cb34f1936bd230c135d888e3ef8e59cb2
임혜선 <57754902+HyeseonLikesPizza@users.noreply.github.com>
2026-03-27 21:57:41 +0900

    [Bronze V] Title: 노트북 세 대를 가지고 왔다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33515. 노트북 세 대를 가지고 왔다/노트북 세 대를 가지고 왔다.cc
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <cstdio>
+#include <stdio.h>
+#include <climits>
+#include <queue>
+#include <tuple>
+#include <cmath>
+#include <map>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+
+int main()
+{
+    cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+
+    long N, B;
+
+    cin >> N >> B;
+    if (N < B) cout << N;
+    else cout << B;
+    
+    
+    return 0;
+}
